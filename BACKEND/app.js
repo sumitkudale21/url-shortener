3c52d203878046959130340fa91c42ab25064ad1
sumitkudale21 <sumitkudale3545@gmail.com>
2025-05-31 10:57:46 +0530

    model folder added

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,7 +1,9 @@
 import express from "express";
 import { nanoid } from "nanoid";
 import dotenv from "dotenv";
-import connectDB from "./src/config/mongo.config.js"
+import connectDB from "./src/config/mongo.config.js";
+import ShortUrl from "./src/models/shorturl.js";
+
 
 dotenv.config("./.env")
 const app = express();
@@ -11,8 +13,14 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.post("/api/create",(req,res)=>{
   const {url} = req.body;
-  console.log(url);
-  res.send(nanoid(7))
+  const shortUrl = nanoid(7);
+  const newUrl = new ShortUrl({
+  full_url: url,
+  short_url: shortUrl, // fix this field name to match schema!
+});
+
+  newUrl.save()
+  res.send(nanoid(7))  
 })
 
 app.listen(5000, ()=>{
