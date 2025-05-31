f830b9091b34de796552219bfa16097283a9a112
sumitkudale21 <sumitkudale3545@gmail.com>
2025-05-31 11:03:24 +0530

    add shorturl file

---
+++ BACKEND/src/models/shorturl.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const shortURLSchema = new mongoose.Schema({
+  full_url: {
+    type: String,
+    required: true,
+  },
+  short_url: {
+    type: String,
+    required: true,
+    index: true,
+    unique: true,
+  },
+  clicks: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+const ShortUrl = mongoose.model("ShortUrl", shortURLSchema);
+export default ShortUrl;
